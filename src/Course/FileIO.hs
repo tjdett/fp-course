8fd5a62f7214c9f1b260682c63c68847d6274fa9
Dima Kovalevsky <dima.kovalevsky@tiny.cloud>
2021-07-13 22:31:15 +0300

    Complete FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,45 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
-
+printFile path content = (<*) (putStrLn ("============ " ++ path)) (putStrLn content)
+  
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
+
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles Nil = void (pure Nil :: IO (List ()))
+printFiles (h :. t) = (<*) (printFile (fst h) (snd h)) (printFiles t)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile path = (,) <$> pure path <*> readFile path
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles l = sequence (getFile <$> l)
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run path = do
+  paths <- readFile path
+  files <- getFiles (lines paths)
+  printFiles files
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = run =<< (headOr Nil <$> getArgs)
 
 ----
 
